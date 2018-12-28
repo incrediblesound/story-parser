c2f429201f7aa05bfde24dde80c185bfe62b320d
James Edwards <james.howe.edwards@gmail.com>
2018-12-28 14:30:36 -0800

    Update version

--- tests/test5.js
+++ tests/test5.js
@@ -2,26 +2,23 @@ const parser = require('../main')
 const assert = require('assert')
 
 const testStory = `
-PLAYER "hero"
-HEALTH 20 ATTACK 3 DEFENSE 4
-ITEM "weapon" NAME "sword" DAMAGE 3 SPEED 4
-ITEM "armor" NAME "leather" DEFENSE 1
-MONEY "gold" 20
 
 PAGE 0
 "this place has two exits."
-OPTION 1 "This option goes to page 1"
-OPTION 2 "This option goes to page 2" LOCK "key"
+OPTION 1 "This option goes to page 1" 
+OPTION 2 "This option goes to page 2" IF NOT "sight"
+
+PAGE 1
+"this place has two exits."
+OPTION 0 "This option goes to page 0"
+OPTION 2 "This option goes to page 2"
 
 PAGE 2
 "this place has two exits."
+ITEM "hidden" NAME "sight"
+
 OPTION 0 "This option goes to page 0"
-
-PAGE 1
-"this place has a key."
-ITEM "key" NAME "key"
-
-OPTION 0 "This option goes to page 0"`
+OPTION 1 "This option goes to page 1"`
 
 describe('rich story with combat and rewards text', () => {
   const story = parser(testStory)
